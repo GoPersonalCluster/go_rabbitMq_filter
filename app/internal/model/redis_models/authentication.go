04383df9ee0513e654a7a26df0490034d784a9db
Walter Matsuda <walter-eidi@hotmail.com>
2026-09-13 21:29:03 -0300

    continuado implementação de cache

--- app/internal/model/redis_models/authentication.go
+++ app/internal/model/redis_models/authentication.go
@@ -1,8 +1,12 @@
 package redismodels
 
+import (
+	"time"
+)
 
 type Authentication struct {
-	Email    string `json:"email"`,
-	Score 	 int64 `json:`,
-	
+	Email     string    `json:"email"`
+	Score     float64   `json:"score"`
+	UpdatedAt time.Time `json:"updatedat"`
+	CreatedAt time.Time `json:"createdat"`
 }
