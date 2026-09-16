521ca0c1e019d032d0553b9eaa39be81c47df50f
Walter Matsuda <walter-eidi@hotmail.com>
2026-09-16 20:42:26 -0300

    iniciado estudos de concorrencia para iniciar a implementação injeção de dependencia em nivel de pacote

--- app/internal/tests/di/dependency_injection_test.go
+++ app/internal/tests/di/dependency_injection_test.go
@@ -2,8 +2,15 @@ package di_test
 
 import (
 	"testing"
+
+	"github.com/GoPersonalCluster/go_rabbitMq_filter/app/internal/di"
+	"github.com/stretchr/testify/assert"
 )
 
 func EnsureMultipleCallsToDiUseSameInstance(t *testing.T) {
-
+	dbCon := di.NewDbConnectionDi()
+	print(dbCon.ID)
+	dbCon2 := di.NewDbConnectionDi()
+	print(dbCon2.ID)
+	assert.Equal(t, dbCon, dbCon2)
 }
